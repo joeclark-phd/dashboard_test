d3e599af95bebcbd6cc258cc9c31fd27d06dc964
Joseph Clark <joseph.w.clark@asu.edu>
2016-07-02 21:22:54 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,44 @@
+"""
+Solution to sharoonthomas's fulfil.io challenge.
+Created on July 2, 2016
+"""
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+
+# tutorial routes
+
+@app.route("/")
+def hello():
+    return( "Hello world!" )
+
+@app.route("/dashboard")
+def dashboard():
+    # we get the data from a JSON web service and prepare it for display
+
+    #import requests
+    #import json
+
+    # grabbing JSON from a static test URL because I don't have access to fulfil.io
+    #data = requests.get("http://localhost:5000/testdata")
+    #recs = json.loads(data.content.decode())
+
+    # here we tally the data we'll be plotting
+    #from collections import Counter
+    #states = Counter( [r["shipment_address.subdivision.code"] for r in recs] )
+    
+    return render_template("dashboard.html")
+
+    
+@app.route("/testdata")
+def testdata():
+    # returns the contents of 'response.json' for testing the app 
+    return app.send_static_file("response.json")
+    
+import os
+if __name__ == "__main__":
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
