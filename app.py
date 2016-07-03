82d64eef7180f3d542f8e0910b5343e974d295f7
Joseph Clark <joseph.w.clark@asu.edu>
2016-07-02 21:57:22 -0400

    add code for real fulfil api

--- app.py
+++ app.py
@@ -12,8 +12,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return( "Hello world!" )
+    return( "Hello world!\nGo to /dashboard for the working dashboard with test data.\nOr try /dash/your-api-key to access the real fulfil.io demo data." )
 
+    
+    
 @app.route("/dashboard")
 def dashboard():
     # we get the data from a JSON web service and prepare it for display
@@ -33,7 +35,20 @@ def dashboard():
     
     return render_template("dashboard.html",jslist=jslist)
 
-    
+
+@app.route("/dash/<apikey>")
+def dash(apikey);
+    # as above, but with the real URL given by sharoon, and API key provided
+    url = "https://fulfil_demo.fulfil.io/api/v1/model/sale.sale?field=reference&field=shipment_address.subdivision.code&field=shipment_address.country.code&filter=[[\"state\",\"=\",\"processing\"]]"
+    data = requests.get(url, headers={"x-api-key":apikey})
+
+    # same code as above from here on
+    recs = json.loads(data.content.decode())
+    from collections import Counter
+    states = Counter( [r["shipment_address.subdivision.code"] for r in recs] )
+    jslist = ",".join([ "['{}', {}]".format(i[0],i[1]) for i in states.items()])
+    return render_template("dashboard.html",jslist=jslist)
+
 
     
 import os
